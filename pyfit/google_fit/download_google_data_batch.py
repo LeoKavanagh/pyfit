38bbbecfab37c48c19d2c49009341cff4a7b01d8
leo <leok90@gmail.com>
2019-02-17 23:10:38 +0000

    download google data batch with credentials

--- pyfit/google_fit/download_google_data_batch.py
+++ pyfit/google_fit/download_google_data_batch.py
@@ -1,30 +1,29 @@
 import google.oauth2.credentials
 import google_auth_oauthlib.flow
-import os
-import requests
-from flask import request
-import json
-import datetime as dt
-import pandas as pd
-from authorise import authorize
-from google_fit_utils import (get_steps_request,
-                              get_agg,
-                              get_dataset)
 
-def download_google_data_batch(access_token):
+import datetime as dt
+import pickle
+from googleapiclient.discovery import build
+
+import os
+import json
+from google_fit_utils import get_epoch, get_dataset
+
+def download_google_data_batch(credentials):
     """
-    :param access_token: string. from oath2
+    :param credentials: from oath2. must have refresh token
     :return: None
     """
 
-    # Date the google data starts
+    # Roughly the date the google data starts - 
+    # when I bought the smartwatch
     start_date = dt.date(2018, 8, 1)
 
     end_date =  dt.date.today() - dt.timedelta(days=1)
 
     heart_data_source = ('derived:com.google.heart_rate.bpm:'
                          'com.google.android.gms:merge_heart_rate_bpm')
-    heart = get_dataset(access_token, start_date,
+    heart = get_dataset(credentials, start_date,
                         end_date, heart_data_source)
 
     with open('data/google/google_heart.json', 'w') as f:
@@ -32,7 +31,7 @@ def download_google_data_batch(access_token):
 
     sleep_data_source = ('raw:com.google.activity.'
                          'segment:com.urbandroid.sleep:')
-    sleep = get_dataset(access_token, start_date,
+    sleep = get_dataset(credentials, start_date,
                         end_date, sleep_data_source)
 
     with open('data/google/google_sleep.json', 'w') as f:
@@ -41,8 +40,10 @@ def download_google_data_batch(access_token):
 
 def main():
 
-    access_token = authorize()
-    download_google_data_batch(access_token)
+    with open('/home/leo/repos/pyfit/credentials.pickle', 'rb') as f:
+        credentials = pickle.load(f)
+
+    download_google_data_batch(credentials)
 
 
 if __name__ == "__main__":
