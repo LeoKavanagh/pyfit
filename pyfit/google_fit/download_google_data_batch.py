0ba1c75a4f3f6618df9d934241b004aaf0037b6e
leo <leok90@gmail.com>
2019-05-17 19:59:17 +0100

    download yesterday's google data

--- pyfit/google_fit/download_google_data_batch.py
+++ pyfit/google_fit/download_google_data_batch.py
@@ -9,6 +9,43 @@ import os
 import json
 from google_fit_utils import get_epoch, get_dataset
 
+
+def download_yesterdays_google_heart_data(credentials):
+    """
+    :param credentials: from oath2. must have refresh token
+    :return: None
+    """
+
+    start_date =  dt.date.today() - dt.timedelta(days=2)
+    end_date =  dt.date.today() - dt.timedelta(days=1)
+
+    heart_data_source = ('derived:com.google.heart_rate.bpm:'
+                         'com.google.android.gms:merge_heart_rate_bpm')
+
+    heart = get_dataset(credentials, start_date,
+                        end_date, heart_data_source)
+
+    return heart
+
+
+def download_yesterdays_google_sleep_data(credentials):
+    """
+    :param credentials: from oath2. must have refresh token
+    :return: None
+    """
+
+    start_date =  dt.date.today() - dt.timedelta(days=2)
+    end_date =  dt.date.today() - dt.timedelta(days=1)
+
+    sleep_data_source = ('raw:com.google.activity.'
+                         'segment:com.urbandroid.sleep:')
+
+    sleep = get_dataset(credentials, start_date,
+                        end_date, sleep_data_source)
+
+    return sleep
+
+
 def download_google_data_batch(credentials):
     """
     :param credentials: from oath2. must have refresh token
